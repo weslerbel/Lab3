88cb355ffe481805b0c46808edc96f2981993e34
Weslerbel <weslerbel@mesoamericana.edu.gt>
2020-05-08 21:29:30 -0600

    Se agregaron Clases Propietario y Propiedad con sus debidos atributos

---
+++ L3/Propiedad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L3
+{
+    class Propiedad
+    {
+        private int no_casa;
+        private int dpi_duenio;
+        private double cuota_mantenimiento;
+
+        public int No_casa { get => no_casa; set => no_casa = value; }
+        public int Dpi_duenio { get => dpi_duenio; set => dpi_duenio = value; }
+        public double Cuota_mantenimiento { get => cuota_mantenimiento; set => cuota_mantenimiento = value; }
+
+        public Propiedad()
+        {
+            no_casa = No_casa;
+            dpi_duenio = Dpi_duenio;
+            cuota_mantenimiento = Cuota_mantenimiento;
+        }
+    }
+}
---
+++ L3/Propietario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L3
+{
+    class Propietario
+    {
+        private string dpi;
+        private string nombre;
+        private string apellido;
+
+        public string Dpi { get => dpi; set => dpi = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+
+        public Propietario()
+        {
+            dpi = Dpi;
+            nombre = Nombre;
+            apellido = Apellido;
+        }
+    }
+}
