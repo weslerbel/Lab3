79270f64391fe61eb0f75dede5be2decc741868d
Weslerbel <weslerbel@mesoamericana.edu.gt>
2020-05-09 21:33:20 -0600

    Se agregaron metodos para guardar los datos en texto plano

--- L3/Form1.cs
+++ L3/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,8 @@ namespace L3
         List<Propiedad> propiedades;
         Propietario propietario;
         Propiedad propiedad;
+        public string filename_propietario = @"C:\L3\Propietario.txt";
+        public string filename_propiedades = @"C:\L3\Propiedades.txt";
 
         public Form1()
         {
@@ -31,6 +34,7 @@ namespace L3
             propietario.Nombre = textBox2.Text;
             propietario.Apellido = textBox3.Text;
             propietarios.Add(propietario);
+            GuardarTxtPropietario();
             MostrarDatos();
         }
 
@@ -40,5 +44,68 @@ namespace L3
             dataGridView1.DataSource = propietarios;
             dataGridView1.Refresh();
         }
+
+
+        void GuardarTxtPropietario()
+        {
+            if (!File.Exists(filename_propietario))
+            {
+                Directory.CreateDirectory(@"C:\L3");
+                FileStream stream = new FileStream(filename_propietario, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(stream);
+                foreach (var p in propietarios)
+                {
+                    writer.WriteLine(p.Dpi);
+                    writer.WriteLine(p.Nombre);
+                    writer.WriteLine(p.Apellido);
+                }
+                writer.Close();
+            }
+            else
+            {
+                FileStream stream = new FileStream(filename_propietario, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(stream);
+                foreach (var p in propietarios)
+                {
+                    writer.WriteLine(p.Dpi);
+                    writer.WriteLine(p.Nombre);
+                    writer.WriteLine(p.Apellido);
+                }
+                writer.Close();
+            }
+            MessageBox.Show("Guardado con Exito!", "Confirmacion!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        void GuardarTxtPropiedad()
+        {
+            if (!File.Exists(filename_propiedades))
+            {
+                Directory.CreateDirectory(@"C:\L3");
+                FileStream stream = new FileStream(filename_propiedades, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(stream);
+                foreach (var p in propiedades)
+                {
+                    writer.WriteLine(p.No_casa);
+                    writer.WriteLine(p.Dpi_duenio);
+                    writer.WriteLine(p.Cuota_mantenimiento);
+                }
+                writer.Close();
+            }
+            else
+            {
+                FileStream stream = new FileStream(filename_propiedades, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(stream);
+                foreach (var p in propiedades)
+                {
+                    writer.WriteLine(p.No_casa);
+                    writer.WriteLine(p.Dpi_duenio);
+                    writer.WriteLine(p.Cuota_mantenimiento);
+                }
+                writer.Close();
+            }
+            MessageBox.Show("Guardado con Exito!", "Confirmacion!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
     }
 }
+
